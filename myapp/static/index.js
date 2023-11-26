4428bd6a395679201fe5db282367449624a7b3c4
Asadul Al Galib <asadulgalibvat69@gmail.com>
2023-11-26 22:01:18 +0600

    migrating the whole design to tailwindcss except the forms

--- myapp/static/index.js
+++ myapp/static/index.js
@@ -1,8 +1,40 @@
-navbarMenuEl = document.querySelector('.navbar-menu');
-navbarWrapperEl = document.querySelector('.navbar-wrapper');
+document.addEventListener("DOMContentLoaded", () => {
+  // hover effect for icons in navbar menu items
+  icons = document.querySelectorAll(".navbar-wrapper svg");
+  icons.forEach((icon) => {
+    parent = icon.closest("div");
+    parent.onmouseover = () => {
+      icon.classList.add("fill-teal-900");
+    };
+    parent.onmouseleave = () => {
+      icon.classList.remove("fill-teal-900");
+    };
+  });
+  
+  // animate navbar menu
+  const navbarWapperEl = document.querySelector('.navbar-wrapper');
+  const navbarVerticalEl = document.querySelector('.navbar-vertical');
+  
+  document.getElementById('navbar-menu').onclick = () => {
+    if(navbarWapperEl.style.gridTemplateRows !== '1fr') {
+      navbarWapperEl.style.gridTemplateRows = '1fr';
+      navbarVerticalEl.style.paddingBottom = '1.25rem';
+    }
+    else {
+      navbarWapperEl.style.gridTemplateRows = '0fr';
+      navbarVerticalEl.style.paddingBottom = '0';
+    }
+  };
 
-navbarMenuEl.addEventListener('click', function() {
-    console.log(navbarWrapperEl.style.gridTemplateRows);
-    if (navbarWrapperEl.style.gridTemplateRows !== '1fr') navbarWrapperEl.style.gridTemplateRows = '1fr';
-    else navbarWrapperEl.style.gridTemplateRows = '0fr';
+  // hover effect for icons in buttons
+  icons = document.querySelectorAll('.button svg, input[type="submit"] svg');
+  icons.forEach((icon) => {
+    parent = icon.closest('div');
+    parent.onmouseover = () => {
+      icon.classList.add('fill-white');
+    };
+    parent.onmouseleave = () => {
+      icon.classList.remove('fill-white');
+    }
+  });
 });
